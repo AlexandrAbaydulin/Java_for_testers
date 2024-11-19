3c4f41a9e75d2d9c3c0dbabc53f15dc0f96a4556
Aleksandr <>
2024-11-19 17:52:40 +0300

    Создан класс Triangle и тесты для него

---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -0,0 +1,33 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+
+    @Test
+    void canCalculateArea() {
+        var s = new Triangle(10.0, 10.0, 12.0);
+        double result = s.triangleArea();
+        Assertions.assertEquals(48.0, result);
+    }
+
+    @Test
+    void canCalculatePerimeter() {
+        Assertions.assertEquals(15.0, new Triangle(4.0, 5.0, 6.0).trianglePerimeter());
+    }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(5.0,3.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+}
