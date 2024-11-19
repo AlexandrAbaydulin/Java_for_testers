c07b3f2c2265208570b0034dc86eec310604990b
Aleksandr <>
2024-11-19 16:13:45 +0300

    Добавление объектов

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,11 @@
+import java.io.File;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        var configFile = new File("C:\\Users\\aabaydulin\\Documents\\GitHub\\Java_for_testers\\sandbox\\build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
     }
 }
