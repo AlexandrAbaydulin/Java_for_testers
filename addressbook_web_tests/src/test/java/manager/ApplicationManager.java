2bdd5f200d2e75c66e4b7d06a475479112204cc6
Aleksandr <>
2024-11-20 22:19:25 +0300

    Обновлены тесты для контактов

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,8 +1,10 @@
 package manager;
 
+import model.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -50,6 +52,66 @@ public class ApplicationManager {
         } catch (NoSuchElementException exception) {
             return false;
         }
+    }
+
+    public boolean isContactPresent() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    public void createContact(ContactData contact) {
+        driver.findElement(By.linkText("add new")).click();
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).sendKeys(contact.firstname());
+        driver.findElement(By.name("middlename")).click();
+        driver.findElement(By.name("middlename")).sendKeys(contact.middlename());
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).sendKeys(contact.lastname());
+        driver.findElement(By.name("nickname")).click();
+        driver.findElement(By.name("nickname")).sendKeys(contact.nickname());
+        driver.findElement(By.name("title")).click();
+        driver.findElement(By.name("title")).sendKeys(contact.title());
+        driver.findElement(By.name("company")).click();
+        driver.findElement(By.name("company")).sendKeys(contact.company());
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).sendKeys(contact.address());
+        driver.findElement(By.name("home")).click();
+        driver.findElement(By.name("home")).sendKeys(contact.home());
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys(contact.email());
+        driver.findElement(By.name("homepage")).click();
+        driver.findElement(By.name("homepage")).sendKeys(contact.homepage());
+        driver.findElement(By.name("bday")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bday"));
+            dropdown.findElement(By.xpath("//option[. = '" + contact.bday() + "']")).click();
+        }
+        driver.findElement(By.name("bmonth")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bmonth"));
+            dropdown.findElement(By.xpath("//option[. = '" + contact.bmonth() + "']")).click();
+        }
+        driver.findElement(By.name("byear")).click();
+        driver.findElement(By.name("byear")).sendKeys(contact.byear());
+        driver.findElement(By.name("aday")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("aday"));
+            dropdown.findElement(By.xpath("//option[. = '" + contact.aday() + "']")).click();
+        }
+        driver.findElement(By.name("amonth")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("amonth"));
+            dropdown.findElement(By.xpath("//option[. = '" + contact.amonth() + "']")).click();
+        }
+        driver.findElement(By.name("ayear")).click();
+        driver.findElement(By.name("ayear")).sendKeys(contact.ayear());
+        driver.findElement(By.xpath("//div[@id=\'content\']/form/input[20]")).click();
+        driver.findElement(By.linkText("home page")).click();
+    }
+
+    public void removeContact() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.xpath("//input[@value=\'Delete\']")).click();
+        driver.findElement(By.linkText("home")).click();
     }
 
 }
---
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -0,0 +1,93 @@
+package manager;
+
+import model.ContactData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ContactHelper extends HelperBase {
+
+    public ContactHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+    public void createContact(ContactData contact) {
+        initContactCreation();
+        fillContactForm(contact);
+        submitContactCreation();
+        returnToContactPage();
+    }
+
+    public void modifyContact(ContactData contact, ContactData modifiedContact, int index) {
+        selectContact(contact);
+        initContactModification(index);
+        fillContactForm(modifiedContact);
+        submitContactModification();
+        returnToContactPage();
+    }
+
+    private void fillContactForm(ContactData contact) {
+        type(By.name("firstname"), contact.firstname());
+        type(By.name("middlename"), contact.middlename());
+        type(By.name("lastname"), contact.lastname());
+        type(By.name("nickname"), contact.nickname());
+        type(By.name("title"), contact.title());
+        type(By.name("company"), contact.company());
+        type(By.name("address"), contact.address());
+        type(By.name("home"), contact.home());
+        type(By.name("email"), contact.email());
+        type(By.name("homepage"), contact.homepage());
+        dropdownClick(By.xpath("//select[@name='bday']"), contact.bday());
+        dropdownClick(By.xpath("//select[@name='bmonth']"), contact.bmonth());
+        type(By.name("byear"), contact.byear());
+        dropdownClick(By.xpath("//select[@name='aday']"), contact.aday());
+        dropdownClick(By.xpath("//select[@name='amonth']"), contact.amonth());
+        type(By.name("ayear"), contact.ayear());
+    }
+
+    private void dropdownClick(By locator, String text) {
+        click(locator);
+        WebElement dropdown = manager.driver.findElement(locator);
+        dropdown.findElement(By.xpath("//option[. = '" + text + "']")).click();
+    }
+
+    public void removeContact(ContactData contact) {
+        selectContact(contact);
+        removeSelectedContacts();
+        returnToContactPage();
+    }
+
+    private void submitContactCreation() {
+        click(By.name("submit"));
+    }
+
+    private void initContactCreation() {
+        click(By.linkText("add new"));
+    }
+
+    private void removeSelectedContacts() {
+        click(By.xpath("//input[@value=\'Delete\']"));
+    }
+
+    public boolean isContactPresent() {
+        return manager.isElementPresent(By.name("selected[]"));
+    }
+
+    private void returnToContactPage() {
+        click(By.linkText("home"));
+    }
+
+    private void submitContactModification() {
+        click(By.name("update"));
+    }
+
+    private void initContactModification(int index) {
+        int teg_index = index + 2;
+        click(By.xpath("//tr[" + teg_index + "]/td[8]/a/img[@alt='Edit']"));
+    }
+
+    private void selectContact(ContactData contact) {
+        click(By.name("selected[]"));
+    }
+
+
+}
