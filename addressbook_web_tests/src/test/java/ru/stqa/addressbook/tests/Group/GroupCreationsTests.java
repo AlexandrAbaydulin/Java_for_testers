99d9ed8d7c7335fe227c897128595b5bd792a439
Aleksandr <>
2024-11-22 17:25:32 +0300

    Лекция 5.7

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/Group/GroupCreationsTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/Group/GroupCreationsTests.java
@@ -2,6 +2,7 @@ package ru.stqa.addressbook.tests.Group;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.GroupData;
 import org.junit.jupiter.api.*;
@@ -32,20 +33,20 @@ public class GroupCreationsTests extends TestBase {
 //        }
 //
         // Чтение файла построчно FileReader("groups.json")
-        var json = "";
-        try (var reader = new FileReader("groups.json");
-            var breader = new BufferedReader(reader)
-        ) {
-            var line = breader.readLine();
-            while(line != null) {
-                json = json + line;
-                line = breader.readLine();
-            }
-        }
+//        var json = "";
+//        try (var reader = new FileReader("groups.json");
+//            var breader = new BufferedReader(reader)
+//        ) {
+//            var line = breader.readLine();
+//            while(line != null) {
+//                json = json + line;
+//                line = breader.readLine();
+//            }
+//        }
         // Чтение файла целиком Files.readString(Paths.get("groups.json")
         //var json = Files.readString(Paths.get("groups.json"));
-        ObjectMapper mapper = new ObjectMapper();
-        var value = mapper.readValue(json, new TypeReference<List<GroupData>>() {});
+        var mapper = new XmlMapper();
+        var value = mapper.readValue(new File("groups.xml"), new TypeReference<List<GroupData>>() {});
         result.addAll(value);
         return result;
     }
